566672e48526db6b0235cb3e6f0596628de2fdfc
Kirill Ushakov <cool.ushackov2016@yandex.ru>
2025-11-06 15:50:04 +0300

    Fixed city-current modal

--- walnutmaker/parts/modals/city-current.php
+++ walnutmaker/parts/modals/city-current.php
@@ -3,6 +3,6 @@
 	<div class="wt-modal-description">
 		Мы определили ваш город, как <span class="wt-modal-city-current-name"><?php bloginfo( 'blogname' ); ?></span>.<br>Если это ваш город, нажмите кнопку «Да».
 	</div>
-	<a class="wt-modal-city-current btn btn-default" href="<?php echo site_url(); ?>" data-name="<?php bloginfo( 'blogname' ); ?>">Да, это мой город</a><br>
+	<a href="#" data-fancybox-close class="wt-modal-city-current btn btn-default" data-name="<?php bloginfo( 'blogname' ); ?>">Да, это мой город</a><br>
 	<a href="#" class="city-change-open">Нет, выбрать другой город</a>
 </div>
